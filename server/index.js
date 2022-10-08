d31eefba266180468b3ae9c0491c87fd531204f5
Enes Eren <thenesern@gmail.com>
2022-10-08 21:55:26 +0300

    Socket cors origin changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ app.use(
   cors({
     optionsSuccessStatus: 200,
     credentials: true,
-    origin: `https://asocial-chat-app.herokuapp.com/`,
     exposedHeaders: "*",
   })
 );
@@ -35,7 +34,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "https://asocial.vercel.app/",
+    origin: "https://asocial-chat-app.herokuapp.com",
     credantials: true,
   },
 });
